eb8bcf2ae6004a6ef76e19d949c55781c653b286
Sameh El Zankalony <zankalony@gmail.com>
2024-12-15 17:23:25 +0200

    Add Footer and StatsSection components; integrate StatsComponent with count-up functionality and update page layout
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import ServicesBrief from '@/components/ServicesBrief';
+import StatsSection from '@/components/StatsSection';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ServicesBrief />
+      <StatsSection />
+      <Footer />
     </>
   );
 }
---
+++ components/StatsComponent.tsx
@@ -0,0 +1,28 @@
+'use client';
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+
+function StatsComponent() {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+  return (
+    <div className="stats stats-vertical lg:stats-horizontal shadow">
+      <div className="stat">
+        <div className="stat-title">Users</div>
+        <div className="stat-value">
+          <CountUp end={700} duration={2.5} suffix="+" enableScrollSpy />
+        </div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-title">Customers</div>
+        <div className="stat-value">
+          <CountUp end={1200} duration={2.5} suffix="+" enableScrollSpy />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default StatsComponent;
